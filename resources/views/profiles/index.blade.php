dd2024abdfaba8f910265cd0ebede958d1cfa586
Adrian K Czaplinski <adidc1999@wp.pl>
2017-09-21 21:43:37 +0000

    Index for profile works correctly

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -1,29 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
+  @foreach($threads as $thread)
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Forum Threads</div>
+
+                <div class="panel-heading">{{ $thread['fullname']}} published <a href="#{{ $thread['text-head-link'] }}">{{ $thread['text-head'] }}</a></div>
 
                 <div class="panel-body">
-                  @foreach($threads as $thread)
+
                     <article>
                         <div>
                             <h4>
-                                {{ $thread['fullname'] }}
+
                             </h4>
                         </div>
                         <div class="body">
                             {{ $thread['text-body'] }}
                         </div>
                     </article>
-                    <hr>
-                    @endforeach
+
                 </div>
             </div>
+
         </div>
     </div>
+    @endforeach
 </div>
 @endsection
